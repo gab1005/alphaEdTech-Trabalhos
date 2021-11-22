95bdbc235601df74457d783310c297b5610d27d5
gab1005 <gabrielqwerty1@gmail.com>
2021-11-22 07:14:42 -0300

    tarefa 07 js

---
+++ JavaScript/07-querySelector/codigo/script.js
@@ -0,0 +1,89 @@
+//container user
+const image = document.querySelector(".picture");
+const nameUser = document.querySelector(".name");
+const age = document.querySelector(".age");
+const state = document.querySelector(".state");
+
+image.src = "./public/profile-picture.png";
+nameUser.innerHTML = "Gabriel de Sousa";
+age.innerHTML = "24 anos";
+state.innerHTML = "Fortaleza - CE";
+
+//imagens usadas - lofi
+const imageLofi01 = document.querySelector(".image01");
+imageLofi01.src = "./public/lofi-pictures/lofi01.png";
+
+const imageLofi02 = document.querySelector(".image02");
+imageLofi02.src = "./public/lofi-pictures/lofi02.png";
+
+const imageLofi03 = document.querySelector(".image03");
+imageLofi03.src = "./public/lofi-pictures/lofi03.png";
+
+const imageLofi04 = document.querySelector(".image04");
+imageLofi04.src = "./public/lofi-pictures/lofi04.png";
+
+const imageLofi05 = document.querySelector(".image05");
+imageLofi05.src = "./public/lofi-pictures/lofi05.png";
+
+
+//container Lo-fi , conteudo textual
+const singerName = document.querySelector(".singer-name");
+const musicalGenre = document.querySelector(".musical-genre");
+const playlist = document.querySelector(".playlist");
+
+const descriptionPlaylist = document.querySelector(".description-playlist");
+descriptionPlaylist.innerHTML = "lista de videos de Lo-fi que são meus preferidos."
+
+const paraOuvir = document.querySelector(".para-ouvir");
+paraOuvir.innerHTML = "Para ouvir as playlist, segue-se os links:"
+
+const music01 = document.querySelector(".music01 p");
+music01.innerHTML = "lofi hip hop radio - beats to study/relax to 🐾";
+
+const textImage02 = document.querySelector(".text-image02");
+textImage02.innerHTML = "O lofi é muito popular pelos seus vídeos ao vivo que nunca terminam, como lofi hip hop radio - beats to study/relax to 🐾, mas também existem playlist's e vídeos curtos (20m a 1h) no youtube com musicas lo-fi.";
+
+const textImage03 = document.querySelector(".text-image03");
+textImage03.innerHTML = "O curioso do lofi, é que ele abre para junção com outros estilos, por exemplo, o Brasil tem seu próprio 'flow' no lo-fi.";
+
+const music02 = document.querySelector(".music02 p");
+music02.innerHTML = "coffee shop radio // 24/7 lofi hip-hop beats";
+
+singerName.innerHTML = "Lo-fi";
+musicalGenre.innerHTML = "gênero derivado do hip hop, mas que não  se prende a ele, podendo percorre outros gêneros como jazz e mpb (mpb brasileira mesmo), já q o lofi não  se prende a nacionalidade; a ideia principal é fazer um som relaxante com um bpm nem muito rápido nem muito devagar, com temas sonoros que deem nostalgia, como chiados e estouros que eram comuns em discos de vinil, essa 'sujeira' trás charme pra musica"
+
+//referencias
+const reference = document.querySelector(".reference");
+const referenceLink = document.querySelector(".reference-link");
+
+const reference01 = document.querySelector(".reference01");
+const referenceLink01 = document.querySelector(".reference-link01");
+
+const reference02 = document.querySelector(".reference02");
+const referenceLink02 = document.querySelector(".reference-link02");
+
+const reference03 = document.querySelector(".reference03");
+const referenceLink03 = document.querySelector(".reference-link03");
+
+const reference04 = document.querySelector(".reference04");
+const referenceLink04 = document.querySelector(".reference-link04");
+
+reference.innerHTML = "para mais leitua sobre Lo-fi:";
+referenceLink.innerHTML = "canaltech.com";
+referenceLink.href = "https://canaltech.com.br/musica/especial-o-que-e-musica-lo-fi-e-por-que-ela-explodiu-durante-a-pandemia-163834/";
+
+
+referenceLink01.innerHTML = "lofi hip hop radio - beats to relax/study to";
+referenceLink01.href = "https://www.youtube.com/watch?v=5qap5aO4i9A";
+
+referenceLink02.innerHTML = "Camp Vibes - Lofi hip hop mix ~ Music to put you in a better mood 🔥";
+referenceLink02.href = "https://www.youtube.com/watch?v=R1Q4nz9T8L0";
+
+referenceLink03.innerHTML = "coffee shop radio // 24/7 lofi hip-hop beats";
+referenceLink03.href = "https://www.youtube.com/watch?v=-5KAN9_CzSA";
+
+referenceLink04.innerHTML = "lofi hip hop radio - beats to study/relax to 🐾";
+referenceLink04.href = "https://www.youtube.com/watch?v=7NOSDKb0HlU";
+
+
+
