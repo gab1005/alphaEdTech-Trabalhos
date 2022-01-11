02886ee3f1d79273ff027f39c6cdbe2951b1b26f
gab1005 <gabrielqwerty1@gmail.com>
2022-01-11 18:04:09 -0300

    19-servidores, tentando comunicar front com back por meio do fetch

--- 04-back-end/19-ajax-fetch/codigos04/backend/server.js
+++ 04-back-end/19-ajax-fetch/codigos04/backend/server.js
@@ -1,12 +1,132 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-app.use(cors());
-const configCors = { origin: false };
 
-app.get("/", cors(configCors), (req, res) => {
-    res.send("funciona");
+//configuração do cors para permitir troca de informações em url's semelhantes
+const configCors = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(configCors))
+
+
+
+const Designs = [{
+    "id": 1,
+    "first_name": "Hulda",
+    "last_name": "Banting",
+    "age": 1,
+    "experience": 1
+}, {
+    "id": 2,
+    "first_name": "Ardenia",
+    "last_name": "Ferrar",
+    "age": 2,
+    "experience": 2
+}, {
+    "id": 3,
+    "first_name": "Korrie",
+    "last_name": "Cripwell",
+    "age": 3,
+    "experience": 3
+}, {
+    "id": 4,
+    "first_name": "Julietta",
+    "last_name": "Danby",
+    "age": 4,
+    "experience": 4
+}, {
+    "id": 5,
+    "first_name": "Hildagarde",
+    "last_name": "Bosnell",
+    "age": 5,
+    "experience": 5
+}]
+
+const devFrontEnd = [
+    {
+        "id": 1,
+        "first_name": "Sadye",
+        "last_name": "Esby",
+        "age": 1,
+        "experience": 1
+    }, {
+        "id": 2,
+        "first_name": "Flossi",
+        "last_name": "Negri",
+        "age": 2,
+        "experience": 2
+    }, {
+        "id": 3,
+        "first_name": "Babara",
+        "last_name": "Hasluck",
+        "age": 3,
+        "experience": 3
+    }, {
+        "id": 4,
+        "first_name": "Rudd",
+        "last_name": "Bennallck",
+        "age": 4,
+        "experience": 4
+    }, {
+        "id": 5,
+        "first_name": "Nataniel",
+        "last_name": "Newtown",
+        "age": 5,
+        "experience": 5
+    }]
+
+
+const devBackEnd = [
+    {
+        "id": 1,
+        "first_name": "Ulrick",
+        "last_name": "Raymont",
+        "age": 1,
+        "experience": 1
+    }, {
+        "id": 2,
+        "first_name": "Derry",
+        "last_name": "Basey",
+        "age": 2,
+        "experience": 2
+    }, {
+        "id": 3,
+        "first_name": "Bevvy",
+        "last_name": "Dempster",
+        "age": 3,
+        "experience": 3
+    }, {
+        "id": 4,
+        "first_name": "Amity",
+        "last_name": "Hambatch",
+        "age": 4,
+        "experience": 4
+    }, {
+        "id": 5,
+        "first_name": "Stafford",
+        "last_name": "Mathe",
+        "age": 5,
+        "experience": 5
+    }]
+
+app.get("/", (req, res) => {
+    console.log(req);
+    res.send("pgaina principal esta funcionando");
+});
+
+app.get("/designs", (req, res) => {
+    res.send(Designs);
+});
+
+app.get("/devFrontEnd", (req, res) => {
+    res.send(devFrontEnd);
+});
+
+app.get("/devBackEnd", (req, res) => {
+    res.send(devBackEnd);
 });
 
 app.listen(8080);
--- 04-back-end/19-ajax-fetch/codigos04/frontend/front-script.js
+++ 04-back-end/19-ajax-fetch/codigos04/frontend/front-script.js
@@ -1,12 +1,36 @@
-const urlContatoServidor = "http://localhost:8080";
-const respostaEscrita = document.querySelector(".resposta");
-function solicitacao() {
-    fetch(urlContatoServidor)
-        .then(res => res.text()
-            .then(respostaTexto => {
-                respostaEscrita.innerHTML = respostaTexto;
-                console.log("não  esta escrevendo a resposta da tag p");
-            })
-        )
-        .catch(erro => console.log("erro: " + erro));
+const url = "'http:/localhost:8080'";
+// const tabela = document.getElementById("tabela-resposta");
+// const criarLinhas = document.createElement("tr");
+// const criarColuna = document.createElement("td");
+
+// criarColuna.innerHTML = data;
+// criarLinhas.appendChild(criarColuna);
+// tabela.appendChild(criarLinhas);
+// console.log("data = ", data);
+function home() {
+    fetch(url)
+        .then(res => res.text())
+        .then(resposta => console.log(resposta))
+        .catch(erro => console.log("deu erro em /" + erro));
+}
+
+function designs() {
+    fetch(url + "/designs")
+        .then(res => res.text())
+        .then(resposta => console.log(resposta))
+        .catch(erro => console.log("erro em Designs: " + erro));
+}
+
+function devFrontEnd() {
+    fetch(url + "/devFrontEnd")
+        .then(res => res.text())
+        .then(resposta => console.log(resposta))
+        .catch(erro => console.log("erro em devFrontEnd: " + erro));
+}
+
+function devBackEnd() {
+    fetch(url + "/devBackEnd")
+        .then(res => res.text())
+        .then(resposta => console.log(resposta))
+        .catch(erro => console.log(`erro em devBackEnd: ${erro}`));
 }
