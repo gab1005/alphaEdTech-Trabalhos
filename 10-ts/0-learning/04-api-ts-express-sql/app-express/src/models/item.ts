847ac6cab6644b657bfcfd20b3bcd2d844d95b20
gab1005 <gabrielqwerty1@gmail.com>
2022-06-27 17:13:23 -0300

    tarefa 2 ts

---
+++ 10-ts/0-learning/04-api-ts-express-sql/app-express/src/models/item.ts
@@ -0,0 +1,16 @@
+// type Item = {
+//   id?: number;
+//   nome: string;
+//   descricao: string;
+// };
+// export default Item;
+
+type Item = {
+  id?: number;
+  nome: string;
+  nascimento: string;
+  email: string;
+  cpf: string;
+};
+
+export default Item;
